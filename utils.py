0e9f0db1ce2263fa8f4dc12636b6744e7232cb68
igeor <ioannis.georvasilis@gmail.com>
2023-10-24 14:45:16 +0300

    [Update] Add interpolation function

--- utils.py
+++ utils.py
@@ -1,12 +1,53 @@
 import torch 
 from torchvision import transforms as transforms
 from torchmetrics.image.fid import FrechetInceptionDistance
+from PIL import Image
 
-def batch_to_rgb(batch_gray: torch.Tensor):
-    batch_rgb = batch_gray.repeat(1, 3, 1, 1)  # Duplicate the grayscale channel to create RGB images
-    batch_rgb *= 255  # Scale the images to [0, 255]
-    batch_rgb = batch_rgb.to(torch.uint8) # Convert to 8-bit integers
-    return batch_rgb 
+def interpolate(batch, mode='RGB', size=299):
+    arr = []
+    for img in batch:
+        if img.shape[0] == 1: img = img.repeat(3,1,1)
+        pil_img = transforms.ToPILImage(mode=mode)(img)
+        resized_img = pil_img.resize((299,299), Image.BILINEAR)
+        arr.append(transforms.ToTensor()(resized_img))
+    return torch.stack(arr)
+
+
+def img_to_rgb(batch: torch.Tensor, img_type: str = 'uint8', img_range: tuple = (0, 255)):
+    """
+    Convert a batch of tensor images to the specified data type and pixel scale.
+
+    Args:
+        batch (torch.Tensor): Batch of tensor images.
+        img_type (str): Target data type for the output image, e.g., 'uint8'.
+        img_range (tuple): Range of pixel values for scaling the output, e.g., (0, 255).
+
+    Returns:
+        torch.Tensor: Batch of converted images.
+    """
+    # Ensure the input tensor is in float32 format for scaling
+    batch = batch.float()
+
+    # Scale the pixel values based on the provided img_range
+    min_val, max_val = img_range
+    batch = (batch - batch.min()) / (batch.max() - batch.min())  # Scale to [0, 1]
+    batch = batch * (max_val - min_val) + min_val  # Scale to the specified range
+
+    # Convert the tensor to the specified data type
+    if img_type == 'uint8':
+        batch = batch.clamp(0, 255).byte()
+    elif img_type == 'float32':
+        pass  # No further conversion needed for float32
+    else:
+        raise ValueError("Unsupported img_type. Supported types are 'uint8' and 'float32'.")
+
+    return batch
+
+# Example usage:
+# Assuming you have a batch of images in tensor format, you can use the function like this:
+# converted_images = img_to_rgb(input_batch, img_type='uint8', img_range=(0, 255))
+
+    
 
 def fid_measure(real_images, fake_images):
     fid = FrechetInceptionDistance(feature=64)
