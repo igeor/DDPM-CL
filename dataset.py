c36ba8f63c31c3b895a16b4ca8d76e52ebe2ed0d
igeor <ioannis.georvasilis@gmail.com>
2023-11-14 13:49:58 +0100

    [Update] Load datasets from torchvision datasets with an option of data path

--- dataset.py
+++ dataset.py
@@ -12,47 +12,45 @@ from datasets import load_dataset
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Subset
 
-def init_dataset(dataset_name=None, dataset_path=None, preprocess='default', split=None, labels=None):
-    # Define the default preprocessing function
-    if isinstance(preprocess, str) and preprocess == 'default':
+def init_dataset(
+        dataset_name, 
+        dataset_path=None, 
+        store_path='./data',
+        preprocess=None, 
+        labels=None
+    ):
+    
+    # Define the default preprocessing function if preprocess is None or it different type than transforms.Compose
+    if preprocess is None or type(preprocess) != transforms.Compose:
         preprocess = transforms.Compose([
             transforms.ToTensor(),
             transforms.Lambda(lambda x: x.repeat(3, 1, 1)),  # Repeat single channel to get three channels
             transforms.Resize((32), antialias=None)
         ])
 
-    # if labels is none 
-    if labels is None: labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    DatasetClass = getattr(datasets, dataset_name)
 
-    if split in [None, 'train']:
-        # Check if dataset_name is provided
-        if dataset_name:
-            full_trainset = datasets.MNIST(dataset_name, download=True, train=True, transform=preprocess)
-        # If not, check if dataset_path is provided
-        elif dataset_path:
-            full_trainset = datasets.MNIST(dataset_path, download=False, train=True, transform=preprocess)
-        else:
-            raise ValueError("Either dataset_name or dataset_path must be provided.")
+    # If dataset path not exist (None) define a default path
+    if dataset_path:
+        full_trainset = DatasetClass(dataset_path, download=False, train=True, transform=preprocess)
+    else:
+        full_trainset = DatasetClass(root=store_path, download=True, train=True, transform=preprocess)
 
-        # Filter the data to only include images with the corresponding labels
-        indices = torch.tensor([label in labels for label in full_trainset.targets])
-        trainset = torch.utils.data.Subset(full_trainset, indices.nonzero().squeeze().tolist())
-        print(f'Number of training images: {len(trainset)}')
+    # Filter the data to only include images with the corresponding labels
+    if labels is None: labels = list(range(len(set(full_trainset.targets))))
+    indices = torch.tensor([label in labels for label in full_trainset.targets])
+    trainset = torch.utils.data.Subset(full_trainset, indices.nonzero().squeeze().tolist())
+    print(f'Number of training images: {len(trainset)}')
+    
 
-    if split in [None, 'test']:
-        # Repeat the process for the test data
-        if dataset_name:
-            full_testset = datasets.MNIST(dataset_name, download=True, train=False, transform=preprocess)
-        elif dataset_path:
-            full_testset = datasets.MNIST(dataset_path, download=False, train=False, transform=preprocess)
-        else:
-            raise ValueError("Either dataset_name or dataset_path must be provided.")
+    if dataset_path:
+        full_testset = DatasetClass(dataset_path, download=False, train=False, transform=preprocess)
+    else:
+        full_testset = DatasetClass(root=store_path, download=True, train=False, transform=preprocess)
+        
+    # Filter the data to only include images with the corresponding labels
+    indices = torch.tensor([label in labels for label in full_testset.targets])
+    testset = torch.utils.data.Subset(full_testset, indices.nonzero().squeeze().tolist())
+    print(f'Number of test images: {len(testset)}')
 
-        # Filter the data to only include images with the corresponding labels
-        indices = torch.tensor([label in labels for label in full_testset.targets])
-        testset = torch.utils.data.Subset(full_testset, indices.nonzero().squeeze().tolist())
-        print(f'Number of test images: {len(testset)}')
-
-    if split == 'train': return trainset
-    elif split == 'test': return testset
-    else: return trainset, testset
+    return trainset, testset
