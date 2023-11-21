559c3daa76ec431ffa01bb7d6e59c27910b21072
igeor <ioannis.georvasilis@gmail.com>
2023-11-21 12:38:08 +0100

    [Update] Create a function for selecting preprocess pipeline

--- utils.py
+++ utils.py
@@ -15,3 +15,42 @@ def interpolate(batch, mode='RGB', size=299):
 def gather(consts: torch.Tensor, t: torch.Tensor):
       c = consts.gather(-1, t)
       return c.reshape(-1, 1, 1, 1)
+
+# create function for selecting preprocessing function
+def get_preprocess_function(dataset_name, flip=False, rotate=False):
+    if dataset_name == 'MNIST':
+        preprocess = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.repeat(3, 1, 1)),  # Repeat single channel to get three channels
+            transforms.Resize((32), antialias=None),
+            transforms.Normalize([0.5], [0.5]), # Convert images from (0,1) to (-1, 1)
+        ])
+    elif dataset_name == 'CIFAR10':
+        preprocess = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5]), # Convert images from (0,1) to (-1, 1)
+        ])
+    elif dataset_name == 'CelebA':
+        preprocess = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5]), # Convert images from (0,1) to (-1, 1)
+        ])
+    elif dataset_name == 'LSUN':
+        preprocess = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5]), # Convert images from (0,1) to (-1, 1)
+        ])
+    elif dataset_name == 'FFHQ':
+        preprocess = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5]), # Convert images from (0,1) to (-1, 1)
+        ])
+    else:
+        raise NotImplementedError
+
+    if flip:
+        preprocess.transforms.insert(0, transforms.RandomHorizontalFlip(p=0.5))
+    if rotate:
+        preprocess.transforms.insert(0, transforms.RandomRotation(degrees=15))
+
+    return preprocess
