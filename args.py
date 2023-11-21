9ad550fe579dbf471c017c9e0a5b650946a8b390
igeor <ioannis.georvasilis@gmail.com>
2023-11-21 14:26:45 +0100

    [Update] Refine training script. Separated args from script. Autogenerate fake images.

---
+++ args.py
@@ -0,0 +1,59 @@
+import argparse
+
+def list_of_strings(arg): return arg.split(',')
+def list_of_ints(arg): return [int(x) for x in arg.split(',')]
+
+def parse_args():
+    # Create an argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device", default="cuda:1", help="Device to use (e.g., 'cuda' or 'cpu')")
+
+    parser.add_argument("--dataset_name", default='MNIST', help="Dataset name")
+    parser.add_argument("--dataset_path", default=None, help="Dataset path")
+    parser.add_argument("--target_dir", default='./dataset', help="The target directory to download the dataset")
+    parser.add_argument("--pr_flip", action="store_true", help="Apply random horizontal flip to training set")
+    parser.add_argument("--pr_rotate", action="store_true", help="Apply random rotation to training set")
+    parser.add_argument("--labels", type=list_of_ints, default=[1], help="Labels to train on")
+
+    parser.add_argument("--num_train_timesteps", type=int, default=1_000, help="Number of training timesteps")
+    parser.add_argument("--beta_start", type=float, default=0.0001, help="Number of training timesteps")
+    parser.add_argument("--beta_end", type=float, default=0.02, help="Number of training timesteps")
+    parser.add_argument("--beta_schedule", default="squaredcos_cap_v2", help="Number of training timesteps")
+    parser.add_argument("--mask", action="store_true", help="Train with masked noise")
+
+    parser.add_argument("--pipeline", default="ddim", help="The pipeline type, 'ddpm' or 'ddim'")
+    parser.add_argument("--num_inference_steps", type=int, default=100, help="The pipeline type, 'ddpm' or 'ddim'")
+
+    parser.add_argument("--image_size", type=int, default=32, help="Image size")
+    parser.add_argument("--in_channels", type=int, default=3, help="The number of input channels, 3 for RGB images")
+    parser.add_argument("--out_channels", type=int, default=3, help="The number of output channels")
+    parser.add_argument("--layers_per_block", type=int, default=2, help="How many ResNet layers to use per UNet block")
+    parser.add_argument("--block_out_channels", type=list_of_ints, 
+                        default=[128, 128, 256, 256, 512, 512], help="The number of output channels for each UNet block")
+    parser.add_argument("--down_block_types", type=list_of_strings, 
+                        default=["DownBlock2D",  "DownBlock2D","DownBlock2D","DownBlock2D","AttnDownBlock2D","DownBlock2D"], 
+                        help="The type of the downsampling block")
+    parser.add_argument("--up_block_types", type=list_of_strings,
+                        default=["UpBlock2D", "UpBlock2D", "UpBlock2D", "UpBlock2D", "AttnUpBlock2D", "UpBlock2D"],
+                        help="The type of the upsampling block")
+
+    parser.add_argument("--num_epochs", type=int, default=100, help="Number of training epochs")
+    parser.add_argument("--train_batch_size", type=int, default=32, help="Batch size for training")
+    parser.add_argument("--eval_batch_size", type=int, default=16, help="Batch size for evaluation")
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=1, help="Number of gradient accumulation steps")
+    parser.add_argument("--learning_rate", type=float, default=1e-4, help="Learning rate")
+    parser.add_argument("--lr_warmup_steps", type=int, default=5, help="Number of learning rate warmup steps")
+
+    parser.add_argument("--pretrained_model_dir", default=None, help="Path to the pretrained model directory")
+    parser.add_argument("--sample_image_epochs", type=int, default=1, help="Number of epochs to save images")
+    parser.add_argument("--generate_image_epochs", type=int, default=100, help="Number of epochs to generate images")
+    parser.add_argument("--n_fake_images", type=int, default=10_000, help="Number of fake images to generate")
+    parser.add_argument("--save_model_epochs", type=int, default=50, help="Number of epochs to save model")
+    parser.add_argument("--output_dir", default="experiment", help="Output directory")
+
+    parser.add_argument("--mixed_precision", default="fp16", help="Mixed precision mode, 'no' for float32, 'fp16' for automatic mixed precision")
+    parser.add_argument("--show_gen_progress", action="store_true", help="Show generation progress")
+    parser.add_argument('--gen_seed', type=int, default=0, help='Seed for generation')
+
+    args = parser.parse_args()
+    return args
--- training.py
+++ training.py
@@ -1,99 +1,61 @@
 import os
 import torch
-import argparse
-from tqdm.auto import tqdm
 import torch.nn.functional as F
 from torchvision import transforms
-from accelerate import Accelerator, notebook_launcher
 from diffusers.utils import make_image_grid
-from diffusers import UNet2DModel, DDPMScheduler, DDPMPipeline, DDIMPipeline
+from diffusers import UNet2DModel, DDIMPipeline, DDPMScheduler, DDPMPipeline
 from diffusers.optimization import get_cosine_schedule_with_warmup
+from tqdm.auto import tqdm
 
 from dataset import init_dataset
+from unet import ClassConditionedUnet
+from utils import get_preprocess_function
+from args import parse_args
 
-def list_of_strings(arg): return arg.split(',')
-def list_of_ints(arg): return [int(x) for x in arg.split(',')]
+""" 
+Initialize the training script
+"""
+args = parse_args()
 
-# Create an argument parser
-parser = argparse.ArgumentParser()
-parser.add_argument("--device", default="cuda", help="Device to use (e.g., 'cuda' or 'cpu')")
-parser.add_argument("--dataset_name", default=None, help="Dataset name")
-parser.add_argument("--dataset_path", default=None, help="Dataset path")
-parser.add_argument("--pipeline", default="ddim", help="The pipeline type, 'ddpm' or 'ddim'")
-parser.add_argument("--pretrained_model_dir", default="No", help="Path to the pretrained model directory")
-parser.add_argument("--labels", type=list_of_ints, default=[2], help="Labels to train on")
-parser.add_argument("--image_size", type=int, default=32, help="Image size")
-parser.add_argument("--train_batch_size", type=int, default=32, help="Batch size for training")
-parser.add_argument("--eval_batch_size", type=int, default=32, help="Batch size for evaluation")
-parser.add_argument("--num_epochs", type=int, default=100, help="Number of training epochs")
-parser.add_argument("--gradient_accumulation_steps", type=int, default=1, help="Number of gradient accumulation steps")
-parser.add_argument("--learning_rate", type=float, default=1e-4, help="Learning rate")
-parser.add_argument("--lr_warmup_steps", type=int, default=5, help="Number of learning rate warmup steps")
-parser.add_argument("--save_image_epochs", type=int, default=50, help="Number of epochs to save images")
-parser.add_argument("--save_model_epochs", type=int, default=50, help="Number of epochs to save model")
-parser.add_argument("--mixed_precision", default="fp16", help="Mixed precision mode, 'no' for float32, 'fp16' for automatic mixed precision")
-parser.add_argument("--output_dir", default="experiment", help="Output directory")
-parser.add_argument("--show_gen_progress", action="store_true", help="Show generation progress")
-parser.add_argument("--train_on", default="train", help="In which dataset to train on (e.g., 'train' or 'test')")
-parser.add_argument('--gen_seed', type=int, default=0, help='Seed for generation')
+# Initialize the output directory
+os.makedirs(args.output_dir, exist_ok=True)
 
-args = parser.parse_args()
+# Store args in the output directory
+if args.output_dir is not None:
+    torch.save(args, os.path.join(args.output_dir, "args.pt"))
 
-# Initialize the output evaluation directory
+# Initialize the directory to save the generated images during training
 eval_dir = os.path.join(args.output_dir, "samples")
 os.makedirs(eval_dir, exist_ok=True)
 
-# Initialize the training dataloader
-preprocess = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Lambda(lambda x: x.repeat(3, 1, 1)),  # Repeat single channel to get three channels
-    transforms.Resize((32), antialias=None),
-    transforms.Normalize([0.5], [0.5]), # Convert images from (0,1) to (-1, 1)
-])
+# Initialize the preprocessing function
+preprocess = get_preprocess_function(
+    args.dataset_name, flip=args.pr_flip, rotate=args.pr_rotate)
 
-if args.dataset_path is not None:
-    train_dataloader = torch.utils.data.DataLoader(
-        init_dataset(dataset_path=args.dataset_path, split=args.train_on, preprocess=preprocess, labels=args.labels), 
-        batch_size=args.train_batch_size, shuffle=True)
-else:
-    train_dataloader = torch.utils.data.DataLoader(
-        init_dataset(dataset_name=args.dataset_name, split=args.train_on, preprocess=preprocess, labels=args.labels), 
-        batch_size=args.train_batch_size, shuffle=True)
-    
+# Get the training and test datasets
+trainset, testset = init_dataset(
+    args.dataset_name, dataset_path=args.dataset_path, target_dir=args.target_dir,
+    labels=args.labels, preprocess=preprocess)
+
+# Initialize the training dataloader
+train_dataloader = torch.utils.data.DataLoader(
+    trainset, batch_size=args.train_batch_size, shuffle=True)
+
 # Define the UNet model
 model = UNet2DModel(
     sample_size=args.image_size,  # the target image resolution
-    in_channels=3,  # the number of input channels, 3 for RGB images
-    out_channels=3,  # the number of output channels
-    layers_per_block=2,  # how many ResNet layers to use per UNet block
-    block_out_channels=(128, 256, 512),  # the number of output channels for each UNet block
-    down_block_types=(
-        "DownBlock2D",  # a regular ResNet downsampling block
-        "DownBlock2D",
-        "DownBlock2D"
-    ),
-    up_block_types=(
-        "UpBlock2D",  # a regular ResNet upsampling block
-        "UpBlock2D",
-        "UpBlock2D"
-    ),
-)
+    in_channels=args.in_channels,  # the number of input channels, 3 for RGB images
+    out_channels=args.out_channels,  # the number of output channels
+    layers_per_block=args.layers_per_block,  # how many ResNet layers to use per UNet block
+    block_out_channels=args.block_out_channels,  # the number of output channels for each UNet block
+    down_block_types=args.down_block_types,
+    up_block_types=args.up_block_types
+).to(args.device)
 
 # Load pretrained model if args.pretrained_model_dir is not None
-if args.pretrained_model_dir != "No":
+if args.pretrained_model_dir is not None:
     model = UNet2DModel.from_pretrained(
         args.pretrained_model_dir, subfolder="unet", use_safetensors=True)
-
-# Move the model to the device
-model = model.to(args.device)
-
-# Define the noise scheduler
-noise_scheduler = DDPMScheduler(num_train_timesteps=1000) # Linear by default
-
-# Load pretrained noise scheduler if args.pretrained_model_dir is not None
-if args.pretrained_model_dir != "No":
-    noise_scheduler = DDPMScheduler.from_pretrained(
-        args.pretrained_model_dir, subfolder="scheduler", use_safetensors=True)
 
 # Define the optimizer 
 optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
@@ -105,111 +67,162 @@ lr_scheduler = get_cosine_schedule_with_warmup(
     num_training_steps=(len(train_dataloader) * args.num_epochs),
 )
 
-# Define the training loop
-def train_loop(model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
-    # Initialize accelerator and tensorboard logging
-    accelerator = Accelerator(
-        mixed_precision=args.mixed_precision,
-        gradient_accumulation_steps=args.gradient_accumulation_steps,
-        log_with="tensorboard",
-        project_dir=os.path.join(args.output_dir, "logs"),
-    )
+# Define the noise scheduler
+noise_scheduler = DDPMScheduler(
+    num_train_timesteps=args.num_train_timesteps,
+    beta_start=args.beta_start,
+    beta_end=args.beta_end,
+    beta_schedule=args.beta_schedule
+)
 
-    # Initialize the repository
-    if accelerator.is_main_process:
-        if args.output_dir is not None:
-            os.makedirs(args.output_dir, exist_ok=True)
-        accelerator.init_trackers("train_log")
+# Load pretrained noise scheduler if args.pretrained_model_dir is not None
+if args.pretrained_model_dir is not None:
+    noise_scheduler = DDPMScheduler.from_pretrained(
+        args.pretrained_model_dir, subfolder="scheduler", use_safetensors=True)
 
-    # Prepare the model, optimizer, and dataloader
-    model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
-        model, optimizer, train_dataloader, lr_scheduler
-    )
 
-    # Initialize the global step
-    global_step = 0
+""" 
+The training loop
+"""
+# Initialize tqdm progress bar
+progress_bar = tqdm(range(args.num_epochs), desc="Epoch 1 | loss: 0.000 | lr: 0.000e+00")
+# Initialize the train log
+train_log = {"loss": [], "lr": [], "step": []}
 
-    # Initialize tqdm progress bar
-    progress_bar = tqdm(range(args.num_epochs), desc="Epoch 1 | loss: 0.000 | lr: 0.000e+00")
+global_step = 0
 
-    # Now you train the model
-    for epoch in range(args.num_epochs):
+for epoch in range(args.num_epochs):
 
-        for clean_images, _ in train_dataloader:
-
-            # Sample gaussian noise 
-            noise = torch.randn(clean_images.shape).to(accelerator.device)
-
-            # Get the batch size
-            bs = clean_images.shape[0]
-
-            # Sample a random timestep for each image
-            timesteps = torch.randint(
-                0, noise_scheduler.config.num_train_timesteps, (bs,), device=accelerator.device
-            ).long()
-
-            # Forward Diffusion Process
-            # Add noise to the clean images according to the noise magnitude at each timestep
-            noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
-            
-            # Backward Diffusion Process
-            with accelerator.accumulate(model):
-                # Predict the noise residual
-                noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
-                # Compute the MSE loss
-                loss = F.mse_loss(noise_pred, noise)
-                # Backpropagate the loss
-                accelerator.backward(loss)
-                # Clip the gradients
-                accelerator.clip_grad_norm_(model.parameters(), 1.0)
-                # Update the model parameters
-                optimizer.step()
-                # Update the learning rate
-                lr_scheduler.step()
-                # Zero the gradients
-                optimizer.zero_grad()
-
-            # Log the loss and learning rate
-            logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
-            accelerator.log(logs, step=global_step)
-            
-            # Update the global step
-            global_step += 1
+    # Iterate over the training batches
+    for batch_idx, (clean_images, _) in enumerate(train_dataloader):
+        # Move the images to the device
+        clean_images = clean_images.to(args.device)
         
-            # Update description of the progress bar
-            progress_bar.set_description(f"Epoch {epoch + 1} | loss: {loss.item():.3f} | lr: {lr_scheduler.get_last_lr()[0]:.3e}")
+        # Sample noise to add to the images
+        noise = torch.randn(clean_images.shape).to(args.device)
 
-        # Update progress bar
-        progress_bar.update(1)
+        # Get the batch size
+        bs = clean_images.shape[0]
 
-        # Sample some demo images with evaluate() and save the model
-        if accelerator.is_main_process:
+        # Sample a random timestep for each image
+        timesteps = torch.randint(
+            0, noise_scheduler.config.num_train_timesteps, (bs,), device=args.device
+        ).long()
 
-            # Evaluate the model 
-            if (epoch + 1) % args.save_image_epochs == 0 or epoch == args.num_epochs - 1:
-                
-                # Initialize the pipeline
-                pipeline = DDIMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
-                if args.pipeline == "ddpm":
-                    pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
-                # Set progress false
-                pipeline.set_progress_bar_config(disable=True)
-                
-                # Generate sample images
+        # Forward Diffusion Process
+        # Add noise to the clean images according to the noise magnitude at each timestep
+        noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+
+        # Backward Diffusion Process
+        # Predict the noise residual
+        noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
+        
+        if args.mask:
+            # Implementation with Masking (1)
+            cond_vector = torch.ones_like(noise)
+            mask = (torch.sum(noise * cond_vector, dim=(1, 2, 3)) > 0).float()
+            # Compute the Masked MSE loss
+            loss = F.mse_loss(noise, noise_pred, reduction="none")
+            loss = torch.mean(loss, dim=(1, 2, 3))
+            loss = torch.mean(loss * mask)
+
+            # # Implementation with Penalty (2)
+            # dot_product = torch.sum(noise_pred * cond_vector, dim=(1, 2, 3))
+            # # Compute the penalty for negative dot products
+            # penalty = torch.relu(-dot_product)
+            # loss = F.mse_loss(noise, noise_pred)
+            # # Combine the main loss and the penalty
+            # loss += 1e-3 * penalty.mean()
+        else:
+            # Compute the MSE loss
+            loss = F.mse_loss(noise, noise_pred)
+
+        # Backpropagate the loss
+        loss.backward()
+        # Clip the gradients
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        # Update the model parameters
+        optimizer.step()
+        # Update the learning rate
+        lr_scheduler.step()
+        # Zero the gradients
+        optimizer.zero_grad()
+
+        # Log the loss and learning rate
+        train_log['loss'] += [loss.item()]
+        train_log['lr'] += [lr_scheduler.get_last_lr()[0]]
+        train_log['step'] += [global_step]
+        global_step += 1
+    
+        # Update description of the progress bar
+        progress_bar.set_description(f"Epoch {epoch + 1} | loss: {loss.item():.3f} | lr: {lr_scheduler.get_last_lr()[0]:.3e}")
+
+    # Update progress bar
+    progress_bar.update(1)
+
+     # Update the train log file
+    torch.save(train_log, os.path.join(args.output_dir, "train_log.pt"))
+
+    # Evaluate the model 
+    if (epoch + 1) % args.sample_image_epochs == 0:
+        
+        with torch.no_grad():
+            
+            # Initialize the pipeline
+            pipeline = DDIMPipeline(unet=model, scheduler=noise_scheduler)
+            if args.pipeline == "ddpm":
+                pipeline = DDPMPipeline(unet=model, scheduler=noise_scheduler)
+                args.num_inference_steps = args.num_train_timesteps
+            
+            # Set progress to false
+            pipeline.set_progress_bar_config(disable=True)
+            
+            # Generate images
+            images = pipeline(
+                batch_size=args.eval_batch_size,
+                # generator=torch.manual_seed(args.gen_seed),
+                num_inference_steps=args.num_inference_steps,
+            ).images
+        
+            # Save the images
+            image_grid = make_image_grid(images[:16], rows=4, cols=4)
+            image_grid.save(f"{eval_dir}/{epoch + 1}.png")
+
+    # Save the model
+    if (epoch + 1) % args.save_model_epochs == 0 or epoch == args.num_epochs - 1:
+        pipeline.save_pretrained(f'{args.output_dir}/epoch-{epoch + 1}')
+    
+    # Generate samples from the model
+    if (epoch + 1) % args.generate_image_epochs == 0 or epoch == args.num_epochs - 1:
+
+        # Initialize output folder
+        output_dir = f"{args.output_dir}/epoch-{epoch + 1}/{args.pipeline}_fake_images"
+        # Create output folder if it doesn't exist
+        os.makedirs(output_dir, exist_ok=True)
+        # List the files in the output folder
+        num_exist_images = len(os.listdir(output_dir))
+
+        # Initialize tqdm bar
+        pbar = tqdm(total=args.n_fake_images, desc=f"Generating fake images for epoch {epoch + 1}...")
+        # Generate images in batches
+        for b_idx in range(0, args.n_fake_images, args.eval_batch_size):
+            # (Inverse Diffusion Process) Sample fake images from random noise
+            # The default pipeline output type is `List[PIL.Image]`
+            with torch.no_grad():
                 images = pipeline(
                     batch_size=args.eval_batch_size,
-                    generator=torch.manual_seed(args.gen_seed),
+                    num_inference_steps=args.num_inference_steps
                 ).images
-                
-                image_grid = make_image_grid(images[:16], rows=4, cols=4)
-                image_grid.save(f"{eval_dir}/{epoch + 1}.png")
 
-            # Save the model
-            if (epoch + 1) % args.save_model_epochs == 0 or epoch == args.num_epochs - 1:
-                pipeline.save_pretrained(f'{args.output_dir}/epoch-{epoch + 1}')
+            # Save the images to the output_dir
+            for image_idx, image in enumerate(images):
+                image.save(f"{output_dir}/{num_exist_images + b_idx + image_idx}.png")
 
-# Run the training loop
-train_loop(model, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
+            # Update tqdm bar
+            pbar.update(args.eval_batch_size)
+    
 
+
+    
 # See results:
 # tensorboard --logdir path/to/your/logs (i.e. results/unetSM/mnist/logs)
